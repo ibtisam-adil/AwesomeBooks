eeefc62464d4e235cf853bf1899b60423a66aba4
ibtisam-adil <ibtisamadil273@gmail.com>
2024-03-10 09:34:55 +0500

    new html styling

--- books.js
+++ books.js
@@ -1,4 +1,3 @@
-
 let booksData = [];
 
 const addBook = document.querySelector(".add-books");
@@ -29,20 +28,36 @@ function renderbooks() {
   booksAdded.innerHTML = booksData
     .map((bookData, index) => {
       return `
-            <p class="title">${bookData.title}</p>
-            <p class="author">${bookData.author}</p>
+        <div class="books">
+            <div class="title-author">
+                <p class="title">"${bookData.title}"</p>
+                <p>by</p>
+                <p class="author">${bookData.author}</p>
+            </div>
             <button class="remove-btn" id="${index}">remove</button>
-            <div class="end-line"></div>
-        `;
+        </div> `;
     })
     .join("");
-    const removebtn = document.querySelectorAll(".remove-btn");
-    removebtn.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-            booksData = booksData.filter((a, index) => {
-                return index !== parseInt(e.target.id);
-            });
-        renderbooks();
-        });
+
+  const removebtn = document.querySelectorAll(".remove-btn");
+  removebtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      booksData = booksData.filter((a, index) => {
+        return index !== parseInt(e.target.id);
+      });
+      renderbooks();
     });
+  });
+
+  if (booksData.length === 0) {
+    document.querySelector(".books-added").style.display = "none";
+    document.querySelector(".end-line").style.display = "none";
+  } else {
+    document.querySelector(".books-added").style.display = "block";
+    document.querySelector(".end-line").style.display = "block";
+  }
 }
+
+
+
+
