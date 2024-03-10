3941a2e212dc6f775c283d606288c0ce5ee65a24
ibtisam-adil <ibtisamadil273@gmail.com>
2024-03-10 11:10:15 +0500

    Created classes

--- books.js
+++ books.js
@@ -1,33 +1,47 @@
-let booksData = [];
+class books {
+  constructor() {
+    this.booksData = [];
 
-const addBook = document.querySelector(".add-books");
+    this.addBook = document.querySelector(".add-books");
+    this.booksAdded = document.querySelector(".books-added");
 
-const booksAdded = document.querySelector(".books-added");
+    this.addBook.addEventListener("submit", (e) => {
+      e.preventDefault();
 
-addBook.addEventListener("submit", (event) => {
-  event.preventDefault();
+      this.title = this.addBook.querySelector(
+        'input[type="text"][placeholder="Title"]'
+      ).value;
 
-  const titleInput = addBook.querySelector(
-    'input[type="text"][placeholder="Title"]'
-  ).value;
-  const authorInput = addBook.querySelector(
-    'input[type="text"][placeholder="Author"]'
-  ).value;
+      this.author = this.addBook.querySelector(
+        'input[type="text"][placeholder="Author"]'
+      ).value;
 
-  let newBook = {
-    title: titleInput,
-    author: authorInput,
-  };
+      this.addBooks();
+    });
+  }
 
-  booksData.push(newBook);
+  addBooks() {
+    let newBook = {
+      title: this.title,
+      author: this.author,
+    };
 
-  renderbooks();
-});
+    this.booksData.push(newBook);
 
-function renderbooks() {
-  booksAdded.innerHTML = booksData
-    .map((bookData, index) => {
-      return `
+    this.renderbooks();
+  }
+
+  removeBook(e) {
+    this.booksData = this.booksData.filter((a, index) => {
+      return index !== parseInt(e.target.id);
+    });
+    this.renderbooks();
+  }
+
+  renderbooks() {
+    this.booksAdded.innerHTML = this.booksData
+      .map((bookData, index) => {
+        return `
         <div class="books">
             <div class="title-author">
                 <p class="title">"${bookData.title}"</p>
@@ -36,28 +50,23 @@ function renderbooks() {
             </div>
             <button class="remove-btn" id="${index}">remove</button>
         </div> `;
-    })
-    .join("");
-
-  const removebtn = document.querySelectorAll(".remove-btn");
-  removebtn.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      booksData = booksData.filter((a, index) => {
-        return index !== parseInt(e.target.id);
+      })
+      .join("");
+    const removebtn = document.querySelectorAll(".remove-btn");
+    removebtn.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        this.removeBook(e);
       });
-      renderbooks();
     });
-  });
 
-  if (booksData.length === 0) {
-    document.querySelector(".books-added").style.display = "none";
-    document.querySelector(".end-line").style.display = "none";
-  } else {
-    document.querySelector(".books-added").style.display = "block";
-    document.querySelector(".end-line").style.display = "block";
+    if (this.booksData.length === 0) {
+      document.querySelector(".books-added").style.display = "none";
+      document.querySelector(".end-line").style.display = "none";
+    } else {
+      document.querySelector(".books-added").style.display = "block";
+      document.querySelector(".end-line").style.display = "block";
+    }
   }
 }
 
-
-
-
+const muneeb = new books();
